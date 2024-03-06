5b0cbb55737a6a8a704804aedc53e8fe822f9c83
s4r44d43 <sararez@live.it>
2024-03-06 23:32:43 +0100

    [start] real js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('.titolo');
+const watchBled = document.querySelector('.watch');
+
+const btnright = document.querySelector('.right');
+const btnleft = document.querySelector('.left');
+
+watchBled.classList.add('none');
+
+setTimeout(()=>{
+  title.classList.add('none');
+  watchBled.classList.remove('none');
+},3000 );
+
